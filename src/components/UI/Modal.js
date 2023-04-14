011126edccef797b2224e0d7df4cb876fced9fbe
Arun kumar <arunkumarjr1999@gmail.com>
2023-04-14 22:40:12 +0530

    Second commit

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,9 +1,12 @@
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom";
-import classes from "./Modal.module.css";
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Modal.module.css';
+
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
+
 const ModalOverlay = (props) => {
   return (
     <div className={classes.modal}>
@@ -12,12 +15,12 @@ const ModalOverlay = (props) => {
   );
 };
 
-const Modal = (props) => {
-  const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById('overlays');
 
+const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop  onClose={props.onClose}/>, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
